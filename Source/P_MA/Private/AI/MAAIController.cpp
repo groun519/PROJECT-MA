3b3cc3913251d029829d1c161ad178caf643ab38
groun519 <groun519@gmail.com>
2025-08-19 04:52:02 +0900

    Add AIController

--- Source/P_MA/Private/AI/MAAIController.cpp
+++ Source/P_MA/Private/AI/MAAIController.cpp
@@ -3,18 +3,14 @@
 
 #include "AI/MAAIController.h"
 #include "Character/MACharacter.h"
-#include "AbilitySystemBlueprintLibrary.h"
-#include "AbilitySystemComponent.h"
-#include "BehaviorTree/BlackboardComponent.h"
-#include "GAS/MAAbilitySystemStatics.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
 
 AMAAIController::AMAAIController()
 {
-	/** Create Objects **//**
+	/** AIPerceptionComponent **//*
 	 * 
-	 * 
+	 *
 	 */
 	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("AI Perception Component");
 	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>("Sight Config");
@@ -31,8 +27,6 @@ AMAAIController::AMAAIController()
 	SightConfig->PeripheralVisionAngleDegrees = 180.f;
 
 	AIPerceptionComponent->ConfigureSense(*SightConfig);
-	AIPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AMAAIController::TargetPerceptionUpdated);
-	AIPerceptionComponent->OnTargetPerceptionForgotten.AddDynamic(this, &AMAAIController::TargetForgotten);
 }
 
 void AMAAIController::OnPossess(APawn* NewPawn)
@@ -44,102 +38,5 @@ void AMAAIController::OnPossess(APawn* NewPawn)
 	if (PawnTeamInterface)
 	{
 		PawnTeamInterface->SetGenericTeamId(GetGenericTeamId());
-	}
-}
-
-void AMAAIController::BeginPlay()
-{
-	Super::BeginPlay();
-	RunBehaviorTree(BehaviorTree);
-}
-
-void AMAAIController::TargetPerceptionUpdated(AActor* TargetActor, FAIStimulus Stimulus)
-{
-	if (Stimulus.WasSuccessfullySensed())
-	{
-		if (!GetCurrentTarget())
-		{
-			SetCurrentTarget(TargetActor);
-		}
-	}
-	else
-	{
-		ForgetActorIfDead(TargetActor);
-	}
-}
-
-void AMAAIController::TargetForgotten(AActor* ForgottenActor)
-{
-	if (!ForgottenActor)
-		return;
-
-	if (GetCurrentTarget() == ForgottenActor)
-	{
-		SetCurrentTarget(GetNextPerceivedActor());
-	}
-}
-
-AActor* AMAAIController::GetNextPerceivedActor() const
-{
-	if (PerceptionComponent)
-	{
-		TArray<AActor*> Actors;
-		AIPerceptionComponent->GetPerceivedHostileActors(Actors);
-
-		if (Actors.Num() != 0)
-		{
-			return Actors[0];
-		}
-	}
-
-	return nullptr;
-}
-
-void AMAAIController::ForgetActorIfDead(AActor* ActorToForget)
-{
-	const UAbilitySystemComponent* ActorASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(ActorToForget);
-	if (!ActorASC)
-		return;
-
-	if (ActorASC->HasMatchingGameplayTag(UMAAbilitySystemStatics::GetDeadStatTag()))
-	{
-		for (UAIPerceptionComponent::TActorPerceptionContainer::TIterator Iter = AIPerceptionComponent->GetPerceptualDataIterator(); Iter; ++Iter)
-		{
-			if (Iter->Key != ActorToForget)
-			{
-				continue;
-			}
-
-			for (FAIStimulus& Stimuli : Iter->Value.LastSensedStimuli)
-			{
-				Stimuli.SetStimulusAge(TNumericLimits<float>::Max());
-			}
-		}
-	}
-}
-
-const UObject* AMAAIController::GetCurrentTarget() const
-{
-	const UBlackboardComponent* BlackboardComponent = GetBlackboardComponent();
-	if (BlackboardComponent)
-	{
-		return GetBlackboardComponent()->GetValueAsObject(TargetBlackboardKeyName);
-	}
-	return nullptr;
-}
-
-void AMAAIController::SetCurrentTarget(AActor* NewTarget)
-{
-	UBlackboardComponent* BlackboardComponent = GetBlackboardComponent();
-	if (!BlackboardComponent)
-		return;
-
-	if (NewTarget)
-	{
-		BlackboardComponent->SetValueAsObject(TargetBlackboardKeyName, NewTarget);
-	}
-	else
-	{
-		BlackboardComponent->ClearValue(TargetBlackboardKeyName);
 	}
 }
