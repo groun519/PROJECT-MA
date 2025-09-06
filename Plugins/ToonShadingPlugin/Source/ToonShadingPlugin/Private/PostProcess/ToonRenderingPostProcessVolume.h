4a15267dc7868105da11d2938c3c629df74d4ed7
groun519 <groun519@gmail.com>
2025-09-06 18:05:55 +0900

    Add ToonShading Plugin

---
+++ Plugins/ToonShadingPlugin/Source/ToonShadingPlugin/Private/PostProcess/ToonRenderingPostProcessVolume.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/PostProcessVolume.h"
+#include "ToonRenderingPostProcessVolume.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AToonRenderingPostProcessVolume : public APostProcessVolume
+{
+	GENERATED_BODY()
+	
+	AToonRenderingPostProcessVolume();
+};
