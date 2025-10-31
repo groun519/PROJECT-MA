bfc176194adfd18e033aee826c07cfd0dc8d651c
alstjr4171 <80445950+alstjr4171@users.noreply.github.com>
2025-11-01 00:22:02 +0900

    Medium Golem Skill Fix
    Ground Attack 스킬 추가 // 데미지 및 나이아가라 완료

--- Source/P_MA/Private/AI/Golem/GA_GroundAttack.h
+++ Source/P_MA/Private/AI/Golem/GA_GroundAttack.h
@@ -2,15 +2,15 @@
 
 #include "CoreMinimal.h"
 #include "GAS/MAGameplayAbility.h"
-#include "GA_ChargeSmash.generated.h"
+#include "GA_GroundAttack.generated.h"
 
 UCLASS()
-class UGA_ChargeSmash : public UMAGameplayAbility
+class UGA_GroundAttack : public UMAGameplayAbility
 {
 	GENERATED_BODY()
 
 public:
-	UGA_ChargeSmash();
+	UGA_GroundAttack();
 
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
 		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
@@ -18,6 +18,8 @@ public:
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
 		const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
+	static FGameplayTag GetComboTargetEventTag();
+	
 protected:
 	UFUNCTION()
 	void OnMontageCompleted();
@@ -25,9 +27,18 @@ protected:
 	UFUNCTION()
 	void OnEndEventReceived(FGameplayEventData Payload);
 
+	UFUNCTION()
+	void HitTarget(FGameplayEventData Data);
+
 	UPROPERTY(EditDefaultsOnly, Category = "Animation")
 	UAnimMontage* ChargeSmashMontage;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Effect")
 	TSubclassOf<UGameplayEffect> DamageEffect;
+
+private:
+	TSubclassOf<UGameplayEffect> GetDamageEffectForCurrentCombo() const;
+	
+	UPROPERTY()
+	TArray<AActor*> IgnoreTargets;
 };
